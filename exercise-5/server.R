51d1e05aa6efeb84026d03c070f4e84cd38fc16b
Gavin Cai <caigavin@gmail.com>
2017-05-17 19:17:24 -0700

    Getting error message argument of length 0

--- exercise-5/server.R
+++ exercise-5/server.R
@@ -1,11 +1,13 @@
 #INFO 201 M18 Exercise 5 Server
 
-#Require the following: shiny, HSUAR, dyplr, and ggplot2
-
+#Require the following: shiny, HSAUR, dyplr, and ggplot2
+library(shiny)
+library(HSAUR)
+library(dplyr)
+library(ggplot2)
 
 #Load the dataset 'womensrole' from the HSUAR package (same way you would load a built in dataset)
-
-
+data(womensrole)
 
 #Here you will define the data that is shown based on your inputs defined in the UI
 #and turn it into an output plot
@@ -22,17 +24,30 @@ shinyServer(function(input, output) {
     
     
     #Filter the dataset based on whether the input is Men, Women, or Both
-
+    plot.data <- womensrole
+    if (input$sex == "Men") {
+      plot.data <- womensrole %>% filter(sex == "Male")
+    } else if (input$sex == "Women") {
+      plot.data <- womensrole %>% filter(sex == "Female")
+    } else {
+      plot.data <- womensrole
+    }
   
     
     #Define a y axis value based on the input Agree or Disagree
-
+    thoughts <- plot.data$disagree
+    if (input$thoughs == "Agree") {
+      thoughts <- plot.data$agree
+    } else {
+      thoughts <- plot.data$disagree
+    }
     
     #Using the values you just defined, construct a scatterplot using ggplot2
     #x will be defined by education, y by agree/disagree
     #Bonus: Factor the color field by sex 
     #(this will allow you to visually see the difference between Men and Women when Both are selected)
-
+    ggplot(data = plot.data, aes(x = education, y = thoughts, color = factor(sex))) + geom_point()
+    
     
   })
   
